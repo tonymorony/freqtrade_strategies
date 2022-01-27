88792300777502051433d28b03412510936a425a
Anton "TonyL" Lysakov <tlysakov@gmail.com>
2022-01-28 04:19:20 +0700

    check diff

--- market_ripper_v1.py
+++ market_ripper_v1.py
@@ -15,47 +15,15 @@ from functools import reduce
 
 """
 Idea of strategy is to sit patiently in fiat when market is decreasing, wait for bull market, sell high and then repeat
-|   Best |  162/1000 |     1111 |    871  164   76 |        5.10% |  205297.110 USDT (20,529.71%) | 4 days 00:17:00 |    -5.39543 |     89343.994 USDT   (30.22%) |
-
-    # Buy hyperspace params:
-    buy_params = {
-        "buy_ema_period": 67,
-        "buy_when_btc_rise": True,
-    }
-
-    # Sell hyperspace params:
-    sell_params = {
-        "sell_ema_period": 275,
-        "sell_rsi": 98,
-        "sell_when_btc_drop": True,
-    }
-
-    # ROI table:
-    minimal_roi = {
-        "0": 0.813,
-        "620": 0.242,
-        "2298": 0.115,
-        "5774": 0
-    }
-
-    # Stoploss:
-    stoploss = -0.348
-
-    # Trailing stop:
-    trailing_stop = True  # value loaded from strategy
-    trailing_stop_positive = 0.01  # value loaded from strategy
-    trailing_stop_positive_offset = 0.08  # value loaded from strategy
-    trailing_only_offset_is_reached = True  # value loaded from strategy
-    
 """
 class tonyl_strategy_3(IStrategy):
 
     INTERFACE_VERSION = 2
 
     buy_when_btc_rise = CategoricalParameter([True, False], default=True, space="buy")
-    sell_when_btc_drop = CategoricalParameter([True, False], default=True, space="sell")
+    sell_when_btc_drop = CategoricalParameter([True, False], default=False, space="sell")
 
-    sell_rsi = IntParameter(70, 99, default=79, space="sell")
+    sell_rsi = IntParameter(70, 99, default=78, space="sell")
     sell_rsi_enabled = CategoricalParameter([True, False], default=True, space="sell")
 
     buy_ema_period = IntParameter(10, 365, default=299, space="buy")
@@ -65,15 +33,15 @@ class tonyl_strategy_3(IStrategy):
 
     # ROI table:
     minimal_roi = {
-        "0": 0.144,
-        "42": 0.107,
-        "189": 0.051,
-        "405": 0
+        "0": 0.392,
+        "643": 0.311,
+        "1815": 0.103,
+        "6141": 0
     }
 
     # Optimal stoploss designed for the strategy.
     # This attribute will be overridden if the config file contains "stoploss".
-    stoploss = -0.31
+    stoploss = -0.349
 
     # Trailing stop:
     trailing_stop = True
@@ -120,7 +88,6 @@ class tonyl_strategy_3(IStrategy):
 
         for val in self.sell_ema_period.range:
             informative[f'sell_ema_{val}'] = ta.EMA(informative, timeperiod=val)
-
 
         dataframe = merge_informative_pair(dataframe, informative, self.timeframe, '1d', ffill=True)
 
